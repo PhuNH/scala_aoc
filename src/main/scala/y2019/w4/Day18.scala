d688657453d1270fa1df93af13843b0faea9856f
PhuNH <1079742+PhuNH@users.noreply.github.com>
2020-05-21 16:36:52 +0200

    Start day 18

---
+++ src/main/scala/y2019/w4/Day18.scala
@@ -0,0 +1,14 @@
+package y2019.w4
+
+import common.Day
+import common.Utils._
+
+class Day18 extends Day(inputPath(2019, 18)) {
+  private val Entrance = '@'
+  private val Passage = '.'
+  private val Wall = '#'
+  
+  def one: Unit = {}
+
+  def two: Unit = {}
+}
