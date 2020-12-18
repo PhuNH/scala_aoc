b668e1578e9ebaf672d8b9f0035f73b17c1dfa83
PhuNH <1079742+PhuNH@users.noreply.github.com>
2020-12-18 12:04:30 +0100

    Make day 17 2020 better

--- src/main/scala/y2020/w3/Day17.scala
+++ src/main/scala/y2020/w3/Day17.scala
@@ -3,54 +3,39 @@ package y2020.w3
 import common.Day
 import common.Utils._
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 class Day17 extends Day(inputPath(2020, 17), testPath(2020, 17, 1)) {
-  type Point = (Int, Int, Int, Int)
-  type Grid = Array[Array[Array[Array[Char]]]]
-  private val Active = '#'
-  private val Inactive = '.'
+  case class Cube(x: Int, y: Int, z: Int = 0, t: Int = 0)
 
-  private val coreLayer: Array[Array[Char]] = using(Source.fromResource(inputs(0)))(
-    _.getLines().toArray.map(_.toCharArray))
-  private val initialGrid = Array.fill(13)(Array.fill(13)(Array.fill(coreLayer.length+12)(Array.fill(coreLayer(0).length+12)(Inactive))))
-  for (i <- coreLayer.indices) for (j <- coreLayer(0).indices) initialGrid(6)(6)(6+i)(6+j) = coreLayer(i)(j)
+  private val space: Set[Cube] = using(Source.fromResource(inputs(0)))(
+    _.getLines().toIndexedSeq.zipWithIndex.flatMap(e => e._1.zipWithIndex.flatMap {
+      case ('#', x) => Some(Cube(x, e._2))
+      case _ => None
+    })).toSet
 
-  private def neighbors(p: Point): Array[Point] = (for {
-    i <- -1 to 1
-    j <- -1 to 1
-    k <- -1 to 1
-    l <- -1 to 1
-    if i != 0 || j != 0 || k != 0 || l != 0
-  } yield (p._1+i, p._2+j, p._3+k, p._4+l)).toArray
-
-  private def atCoords(cubes: Grid, p: Point): Char = {
-    if (p._4 < 0 || p._4 >= cubes.length || p._3 < 0 || p._3 >= cubes(0).length
-      || p._2 < 0 || p._2 >= cubes(0)(0).length || p._1 < 0 || p._1 >= cubes(0)(0)(0).length) Inactive
-    else cubes(p._4)(p._3)(p._2)(p._1)
+  private def neighbors(c: Cube, fourD: Boolean): IndexedSeq[Cube] = {
+    val r = -1 to 1
+    for { i <- r; j <- r; k <- r; l <- if (fourD) r else Seq(0); if Seq(i, j, k, l).exists(_ != 0) }
+      yield Cube(c.x+i, c.y+j, c.z+k, c.t+l)
   }
 
-  private def applyRules(prevCubes: Grid, cubes: Grid, p: Point): Unit = {
-    val activeNeighbors = neighbors(p).map(atCoords(prevCubes, _)).count(_ == Active)
-    if (atCoords(prevCubes, p) == Active && (activeNeighbors != 2 && activeNeighbors != 3)) cubes(p._4)(p._3)(p._2)(p._1) = Inactive
-    else if (atCoords(prevCubes, p) == Inactive && activeNeighbors == 3) cubes(p._4)(p._3)(p._2)(p._1) = Active
+  private def applyRules(c: Cube, space: Set[Cube], fourD: Boolean): Boolean = {
+    val activeNeighbors = neighbors(c, fourD).count(space.contains)
+    if (space.contains(c)) activeNeighbors == 2 || activeNeighbors == 3
+    else activeNeighbors == 3
   }
 
-  private def cloneGrid(from: Grid, to: Grid): Unit =
-    for (l <- to.indices) for (k <- to(0).indices) for (i <- to(0)(0).indices) to(l)(k)(i) = from(l)(k)(i).clone()
-
-  def one: Int = {
-    val curGrid = Array.fill(13)(Array.fill(13)(new Array[Array[Char]](coreLayer.length+12)))
-    cloneGrid(initialGrid, curGrid)
-    val prevGrid = Array.fill(13)(Array.fill(13)(new Array[Array[Char]](coreLayer.length+12)))
-    cloneGrid(initialGrid, prevGrid)
-    for (t <- 1 to 6) {
-      for (l <- -t until 1+t) for (k <- -t until 1+t) for (i <- -t until coreLayer.length+t) for (j <- -t until coreLayer(0).length+t)
-        applyRules(prevGrid, curGrid, (6+j, 6+i, 6+k, 6+l))
-      cloneGrid(curGrid, prevGrid)
-    }
-    curGrid.map(_.map(_.map(_.count(_ == Active)).sum).sum).sum
+  @tailrec
+  private def simulate(space: Set[Cube], steps: Int, fourD: Boolean = false): Set[Cube] = {
+    val neighborSpace = space.flatMap(neighbors(_, fourD))
+    val nextSpace = neighborSpace.filter(applyRules(_, space, fourD))
+    if (steps == 1) nextSpace
+    else simulate(nextSpace, steps-1, fourD)
   }
 
-  def two: Unit = {}
+  def one: Int = simulate(space, 6).size
+
+  def two: Int = simulate(space, 6, fourD = true).size
 }
