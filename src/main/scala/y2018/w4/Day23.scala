e4cc36c6b4c0fb8d9be30a34a828c0302f13039a
PhuNH <1079742+PhuNH@users.noreply.github.com>
2021-10-18 15:54:04 +0200

    On day 23 2018
    A faster way to solve part 2 example :P

--- src/main/scala/y2018/w4/Day23.scala
+++ src/main/scala/y2018/w4/Day23.scala
@@ -26,12 +26,29 @@ class Day23 extends Day(inputPath(2018, 23)) {
   }
 
   private case class Nanobot(pos: Vector, r: Int) {
-    def ranges: Set[Vector] = (for {
-      i <- -r + pos.x to r + pos.x
-      j <- -r + pos.y to r + pos.y
-      k <- -r + pos.z to r + pos.z
-      if (Vector(i,j,k) - pos).manhattan <= r
-    } yield Vector(i, j, k)).toSet
+    val (min_x, max_x): (Int, Int) = (pos.x - r, pos.x + r)
+    val (min_y, max_y): (Int, Int) = (pos.y - r, pos.y + r)
+    val (min_z, max_z): (Int, Int) = (pos.z - r, pos.z + r)
+
+    def isInRange(otherPos: Vector): Boolean = otherPos.manhattan(pos) <= r
+  }
+
+  private object Nanobot {
+    def overlappedRange(indices: IndexedSeq[Int]): Set[Vector] = {
+      val min_x = indices.map(nanobots(_).min_x).max
+      val max_x = indices.map(nanobots(_).max_x).min
+      val min_y = indices.map(nanobots(_).min_y).max
+      val max_y = indices.map(nanobots(_).max_y).min
+      val min_z = indices.map(nanobots(_).min_z).max
+      val max_z = indices.map(nanobots(_).max_z).min
+      (for {
+        x <- min_x to max_x
+        y <- min_y to max_y
+        z <- min_z to max_z
+        p = Vector(x, y, z)
+        if indices.forall(nanobots(_).isInRange(p))
+      } yield p).toSet
+    }
   }
 
   private val nanobots: Array[Nanobot] = using(Source.fromResource(inputs(0)))(_.getLines().toArray.map(l => {
@@ -41,46 +58,23 @@ class Day23 extends Day(inputPath(2018, 23)) {
     Nanobot(pos, r)
   }))
 
-  private def inRangeOf(nanobot: Nanobot): Array[Nanobot] = nanobots.filter(_.pos.manhattan(nanobot.pos) <= nanobot.r)
+  private def inRangeOf(nanobot: Nanobot): Array[Nanobot] = nanobots.filter(b => nanobot.isInRange(b.pos))
 
   def one: Int = {
     val strongest = nanobots.maxBy(_.r)
     inRangeOf(strongest).length
   }
 
-  private def region(group: IndexedSeq[Int], nanobotRanges: Array[Set[Vector]],
-                     groups: Map[Set[Int], Set[Vector]]): Set[Vector] =
-    if (groups.contains(group.toSet)) groups(group.toSet)
-    else {
-      val ranges = group.map(nanobotRanges(_))
-      val result = ranges.reduce(_ & _)
-      result
-    }
-
   @tailrec
-  private def find(groups: Map[Set[Int], Set[Vector]], step: Int, seenGroups: Map[Set[Int], Set[Vector]],
-                   nanobotIndices: Set[Int], nanobotRanges: Array[Set[Vector]]): Int = {
-    val newGroups = groups.flatMap { case (groupIndices, groupRegion) =>
-      val leftIndices = nanobotIndices &~ groupIndices
-      val addedIndexGroups =
-        if (step == 1) leftIndices.toArray.map(Array(_))
-        else leftIndices.toArray.combinations(step).filter(g => seenGroups.contains(g.toSet)).toArray
-      addedIndexGroups
-        .map(addedIndices => (groupIndices ++ addedIndices.toSet,
-                              groupRegion & region(addedIndices, nanobotRanges, seenGroups)))
-        .filter(_._2.nonEmpty).toMap
+  private def largestCombination(size: Int): Set[Vector] = {
+    println(size)
+    nanobots.indices.combinations(size).find(c => Nanobot.overlappedRange(c).nonEmpty) match {
+      case None => largestCombination(size - 1)
+      case Some(c) => Nanobot.overlappedRange(c)
     }
-    if (newGroups.nonEmpty) find(newGroups, step, seenGroups ++ newGroups, nanobotIndices, nanobotRanges)
-    else if (step > 1) find(groups, step / 2, seenGroups, nanobotIndices, nanobotRanges)
-    else groups.values.flatten.map(_.manhattan).min
   }
 
   def two: Int = {
-    val nanobotRanges = nanobots.map(_.ranges)
-    val nanobotIndices = nanobots.indices.toSet
-    val groupsWithEmpty = nanobotRanges.indices.combinations(2)
-      .map(g => (g.toSet, region(g, nanobotRanges, Map.empty))).toArray
-    val groups = groupsWithEmpty.filter(_._2.nonEmpty).toMap
-    find(groups, 2, groups, nanobotIndices, nanobotRanges)
+    largestCombination(nanobots.length).minBy(_.manhattan).manhattan
   }
 }
