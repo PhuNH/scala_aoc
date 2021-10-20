68c51cb8450069bd7dadb5772cb8535f2d5b55d8
PhuNH <1079742+PhuNH@users.noreply.github.com>
2021-10-20 20:05:01 +0200

    Day 17 2018
    Follow and fix a bug in the elegant solution of Florian Cassayre

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # Scala AoC
 Advent of Code 2015, 2018, 2019, 2020 in Scala
 
-2016, 2017: [Rust](https://github.com/PhuNH/rust_aoc)  
-
-### TODO
-2018: 17+
+2016, 2017: [Rust](https://github.com/PhuNH/rust_aoc)
--- src/main/scala/y2018/w4/Day17.scala
+++ src/main/scala/y2018/w4/Day17.scala
@@ -9,12 +9,12 @@ import scala.io.Source
 class Day17 extends Day(inputPath(2018, 17)) {
   private val source = Coords(0, 500)
   private val regex = """([xy])=(\d+), ([xy])=(\d+)\.\.(\d+)""".r
-  private val clays = using(Source.fromResource(inputs(0)))(
+  private val clay = using(Source.fromResource(inputs(0)))(
     _.getLines().flatMap {
       case regex("x", x, "y", y1, y2) => (y1.toInt to y2.toInt).map(Coords(_, x.toInt))
       case regex("y", y, "x", x1, x2) => (x1.toInt to x2.toInt).map(Coords(y.toInt, _))
     }.toSet)
-  private val (smallestY, largestY) = (clays.map(_.v).min, clays.map(_.v).max)
+  private val (smallestY, largestY) = (clay.map(_.v).min, clay.map(_.v).max)
 
   @tailrec
   private def flow(buffer: Set[Coords], water: Map[Coords, Boolean]): Map[Coords, Boolean] = {
@@ -24,29 +24,42 @@ class Day17 extends Day(inputPath(2018, 17)) {
     else {
       val below = springs.head.south
       water.get(below) match {
-        case Some(false) => flow(springs.tail, water + (springs.head -> false))
-        case None if !clays.contains(below) => flow(springs.tail + below, water + (springs.head -> false))
+        case Some(false) =>
+          flow(springs.tail, water + (springs.head -> false))
+        case None if !clay.contains(below) =>
+          flow(springs.tail + below, water + (springs.head -> false))
         case _ =>
           def walk(f: Coords => Coords): Seq[Coords] = {
             lazy val stream: LazyList[Coords] = springs.head #:: stream.map(f)
-            stream.takeWhile(p => !clays.contains(p) && (clays.contains(p.south) || water.contains(p.south)))
+            stream.takeWhile(p => !clay.contains(p) && (clay.contains(p.south) || water.get(p.south).contains(true)))
           }
 
           val (left, right) = (walk(_.west), walk(_.east))
-          val sides = Seq(left.last.west, right.last.east).filter(!clays.contains(_))
+          val sides = Seq(left.last.west, right.last.east).filterNot(clay.contains)
           val all = left ++ right ++ sides
 
-          val newSprings = springs.tail.filter(p => !all.contains(p))
-
-          if (sides.nonEmpty) flow(newSprings ++ sides, water ++ all.map(_ -> false))
-          else flow(newSprings + springs.head.north, water ++ all.map(_ -> true))
+          if (sides.nonEmpty) flow(springs.tail ++ sides, water ++ all.map(_ -> false))
+          else flow(springs.tail + springs.head.north, water ++ all.map(_ -> true))
       }
     }
   }
 
-  private val water = flow(Set(source), Map.empty).filter(w => w._1.v >= smallestY && w._1.v <= largestY).values
+  private def printMap(water: Map[Coords, Boolean]): Unit = {
+    val xRange = (water.keys ++ clay).minBy(_.h).h to (water.keys ++ clay).maxBy(_.h).h
+    (smallestY to largestY).foreach { y =>
+      println(xRange.map(x =>
+        if (clay.contains(Coords(y, x))) '#'
+        else if (water.getOrElse(Coords(y, x), false)) '~'
+        else if (water.contains(Coords(y, x))) '|'
+        else '.').mkString)
+    }
+  }
+//  printMap(Map.empty)
+
+  private val water = flow(Set(source), Map.empty).filter(w => w._1.v >= smallestY && w._1.v <= largestY)
+//  printMap(water)
 
   def one: Int = water.size
 
-  def two: Unit = {}
+  def two: Int = water.count(_._2)
 }
